2608f154c8f7d1abe46c9876b476e4d3117be091
MagentaNyashka <lopatin.lenja@gmail.com>
2025-01-14 13:29:35 +0300

    asadsad

--- main.js
+++ main.js
@@ -2288,7 +2288,7 @@ module.exports.loop = function() {
     // placeBlock('E2N24', {x: 38, y:24}, CORE_BLOCK);
     // placeBlock('E2N24', {x: 38, y:20}, LINK_BLOCK);
     // placeBlock('E2N24', {x: 38, y:20}, MAIN_BLOCK);
-    findPlaceForMainCenter('E2N24');
+    // findPlaceForMainCenter('E2N24');
     // console.log(getRoomPriorityBySourceCount());
     // if(Game.shard.name === 'shard2'){console.log(Math.min(global.getFreeSources('E1N29', global.getSources('E1N29')[0].id).length,2));}   
     // console.log(findClosestHighwayRoom('E1N24'));
