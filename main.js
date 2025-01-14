e7e4411543024a1fe653dcbfcac95e0d8e33428c
MagentaNyashka <lopatin.lenja@gmail.com>
2025-01-14 13:26:16 +0300

    asdasdsd

--- main.js
+++ main.js
@@ -479,7 +479,7 @@ function findPlaceForMainCenter(roomName){
     if(center != null){
         const terrain = new Room.Terrain(roomName);
         for(let x = center.x-2; x < center.x+2; x++){
-            for(let y = center.y-2; x < center.y+2; y++){
+            for(let y = center.y-2; y < center.y+2; y++){
                 if(terrain.get(x,y) === TERRAIN_MASK_WALL || terrain.get(x,y) === TERRAIN_MASK_LAVA){
                     new RoomVisual(roomName).circle(x,y);
                 }
